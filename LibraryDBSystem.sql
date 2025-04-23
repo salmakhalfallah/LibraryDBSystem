c694a523fe326d92b30543d156440b3d08bc86b6
Nachiket Pandit <109383061+nachiketpandit101@users.noreply.github.com>
2025-04-23 00:11:33 -0500

    Added Book, CD, Member, and Loan Tables

--- LibraryDBSystem.sql
+++ LibraryDBSystem.sql
@@ -21,3 +21,45 @@ isAvailable	BOOLEAN NOT NULL,
 
 PRIMARY KEY Lib_Item (itemID, title)
 );
+
+# Creating book and cd tables
+CREATE TABLE Book (
+  ItemID DECIMAL(13) NOT NULL,       
+  Title VARCHAR(45) NOT NULL,    
+  Publisher VARCHAR(255),         
+  Author VARCHAR(255),            
+  PRIMARY KEY (ItemID, Title), 
+  FOREIGN KEY (ItemID, Title) REFERENCES Item(ItemID, Title) 
+);
+
+CREATE TABLE CD (
+  ItemID DECIMAL(13) NOT NULL,        
+  Title VARCHAR(45) NOT NULL,    
+  Director VARCHAR(255),         
+  starActor VARCHAR(255),         
+  PRIMARY KEY (ItemID, Title), 
+  FOREIGN KEY (ItemID, Title) REFERENCES Item(ItemID, Title) 
+);
+
+# Creating Member Table
+CREATE TABLE Member (
+  MemberID INT(6) NOT NULL,       
+  FirstName VARCHAR(255),         
+  LastName VARCHAR(255),          
+  Email VARCHAR(255),             
+  PhoneNumber INT(9),             
+  PRIMARY KEY (MemberID)        
+);
+
+# Creating Loan Table
+CREATE TABLE Loan (
+  LoanID INT(10) NOT NULL AUTO_INCREMENT,   
+  MemberID INT(6) NOT NULL,                 
+  ItemID DECIMAL(13) NOT NULL,                
+  LoanDate DATE,                            
+  DueDate DATE,                             
+  PRIMARY KEY (LoanID),                     
+  FOREIGN KEY (MemberID) REFERENCES Member(MemberID),  
+  FOREIGN KEY (ItemID) REFERENCES Item(ItemID)        
+);
+
