3028aec981f6931f09bbb601e52a03e1252d39cd
Nachiket Pandit <109383061+nachiketpandit101@users.noreply.github.com>
2025-04-26 22:09:26 -0500

    updated to match data dictionary better

--- LibraryDBSystem.sql
+++ LibraryDBSystem.sql
@@ -1,123 +1,108 @@
-# removing librarysystem if exists already in system and creating a new one
+-- Dropping and creating the database
 DROP DATABASE IF EXISTS LIBRARYSYSTEM;
 CREATE DATABASE LIBRARYSYSTEM;
 
-# creating a new library schema
+-- Dropping and creating the schema
 DROP SCHEMA IF EXISTS LIBRARY;
 CREATE SCHEMA LIBRARY;
 
-
 USE LIBRARY;
-SHOW TABLES;
 
-# creating item table
-
-DROP TABLE IF EXISTS ITEM;
-CREATE TABLE ITEM(
-
-itemID	DECIMAL(13) UNIQUE NOT NULL,
-title	VARCHAR(45) NOT NULL,
-genre	SET('Fiction', 'Non-fiction') NOT NULL,
-isAvailable	BOOLEAN NOT NULL,
-
-PRIMARY KEY Lib_Item (itemID, title)
-);
-
-
-
-# Creating book and cd tables
-
-DROP TABLE IF EXISTS BOOK;
-CREATE TABLE BOOK (
-  ItemID DECIMAL(13) NOT NULL,       
-  Title VARCHAR(45) NOT NULL,    
-  Publisher VARCHAR(255),         
-  Author VARCHAR(255),            
-  PRIMARY KEY (ItemID, Title), 
-  FOREIGN KEY (ItemID, Title) REFERENCES ITEM(ItemID, Title) 
-);
-
-DROP TABLE IF EXISTS CD;
-CREATE TABLE CD (
-  ItemID DECIMAL(13) NOT NULL,        
-  Title VARCHAR(45) NOT NULL,    
-  Director VARCHAR(255),         
-  starActor VARCHAR(255),         
-  PRIMARY KEY (ItemID, Title), 
-  FOREIGN KEY (ItemID, Title) REFERENCES ITEM(ItemID, Title) 
-);
-
-
-
-# Creating Member Table
-DROP TABLE IF EXISTS MEMBER;
-CREATE TABLE MEMBER (
-  MemberID INT(6) NOT NULL,       
-  FirstName VARCHAR(255),         
-  LastName VARCHAR(255),          
-  Email VARCHAR(255),             
-  PhoneNumber INT(9),             
-  PRIMARY KEY (MemberID)        
-);
-
-
-# Creating Loan Table
-DROP TABLE IF EXISTS LOAN;
-CREATE TABLE LOAN (
-  LoanID INT(10) NOT NULL AUTO_INCREMENT,   
-  MemberID INT(6) NOT NULL,                 
-  ItemID DECIMAL(13) NOT NULL,                
-  LoanDate DATE,                            
-  DueDate DATE,                             
-  PRIMARY KEY (LoanID),                     
-  FOREIGN KEY (MemberID) REFERENCES MEMBER(MemberID),  
-  FOREIGN KEY (ItemID) REFERENCES ITEM(ItemID)        
-);
-
-
-
-# creating EMPLOYEE table
-DROP TABLE IF EXISTS EMPLOYEE;
-CREATE TABLE EMPLOYEE (
-EmployeeID INT(6) UNIQUE NOT NULL AUTO_INCREMENT,
-DeptNo INT(2) NOT NULL,
-FirstName VARCHAR(255) NOT NULL,
-LastName VARCHAR(255) NOT NULL,
-Email VARCHAR(255),
-PhoneNumber INT(9),
-Salary INT(9),
-Role SET('Volunteer', 'Librarian', 'Maintenance', 'Manager'),
-
-FOREIGN KEY (DeptNo) REFERENCES DEPARTMENT(DeptNo),
-PRIMARY KEY (EmployeeID)
-
-);
-
-
-# creating DEPARTMENT table
-
-DROP TABLE IF EXISTS DEPARTMENT;
-CREATE TABLE DEPARTMENT (
-DeptNo INT(2) UNIQUE NOT NULL AUTO_INCREMENT,
-Location SET('First Floor', 'Second Floor', 'Basement'),
-EmployeeCount INT(3) NOT NULL,
-DeptName VARCHAR(255) UNIQUE NOT NULL,
-
-PRIMARY KEY (DeptNo)
-);
-
-
-# creating EVENTS table 
+-- Drop all tables if they exist
 DROP TABLE IF EXISTS EVENTS;
-CREATE TABLE EVENTS (
-EventID INT(10) UNIQUE NOT NULL AUTO_INCREMENT,
-SupervisorID INT(6) UNIQUE NOT NULL,
-Title VARCHAR(255),
-Location SET('First Floor', 'Second Floor', 'Basement'),
-EDate DATE NOT NULL,
+DROP TABLE IF EXISTS EMPLOYEE;
+DROP TABLE IF EXISTS DEPARTMENT;
+DROP TABLE IF EXISTS LOAN;
+DROP TABLE IF EXISTS MEMBER;
+DROP TABLE IF EXISTS BOOK;
+DROP TABLE IF EXISTS CD;
+DROP TABLE IF EXISTS ITEM;
 
-PRIMARY KEY (EventID),
-CONSTRAINT FK_SupervisorID FOREIGN KEY (SupervisorID) REFERENCES EMPLOYEE(EmployeeID)
+-- Create ITEM table
+CREATE TABLE ITEM (
+  ItemID INT(13) NOT NULL,
+  Title VARCHAR(45) NOT NULL,
+  Genre SET('Fiction', 'Non-fiction') NOT NULL,
+  isAvailable BOOLEAN NOT NULL,
+  PRIMARY KEY (ItemID, Title)
 );
 
+-- Create BOOK table
+CREATE TABLE BOOK (
+  ItemID INT(13) NOT NULL,
+  Title VARCHAR(45) NOT NULL,
+  Publisher VARCHAR(255),
+  Author VARCHAR(255),
+  PRIMARY KEY (ItemID, Title),
+  FOREIGN KEY (ItemID, Title) REFERENCES ITEM(ItemID, Title)
+);
 
+-- Create CD table
+CREATE TABLE CD (
+  ItemID INT(13) NOT NULL,
+  Title VARCHAR(45) NOT NULL,
+  Director VARCHAR(255),
+  starActor VARCHAR(255),
+  PRIMARY KEY (ItemID, Title),
+  FOREIGN KEY (ItemID, Title) REFERENCES ITEM(ItemID, Title)
+);
+
+-- Create MEMBER table
+CREATE TABLE MEMBER (
+  MemberID INT(6) NOT NULL,
+  FirstName VARCHAR(255),
+  LastName VARCHAR(255),
+  Email VARCHAR(255),
+  PhoneNumber INT(9),
+  PRIMARY KEY (MemberID)
+);
+
+-- Create DEPARTMENT table
+CREATE TABLE DEPARTMENT (
+  DeptNo INT(2) NOT NULL AUTO_INCREMENT,
+  Location SET('First Floor', 'Second Floor', 'Basement'),
+  EmployeeCount INT(3) NOT NULL,
+  DeptName VARCHAR(255) UNIQUE NOT NULL,
+  PRIMARY KEY (DeptNo)
+);
+
+-- Create EMPLOYEE table
+CREATE TABLE EMPLOYEE (
+  EmployeeID INT(6) NOT NULL AUTO_INCREMENT,
+  DeptNo INT(2) NOT NULL,
+  FirstName VARCHAR(255) NOT NULL,
+  LastName VARCHAR(255) NOT NULL,
+  Email VARCHAR(255),
+  PhoneNumber INT(9),
+  Salary INT(9),
+  Role SET('Volunteer', 'Librarian', 'Maintenance', 'Manager'),
+  PRIMARY KEY (EmployeeID),
+  FOREIGN KEY (DeptNo) REFERENCES DEPARTMENT(DeptNo)
+);
+
+-- Create LOAN table
+CREATE TABLE LOAN (
+  LoanID INT(10) NOT NULL AUTO_INCREMENT,
+  MemberID INT(6) NOT NULL,
+  ItemID INT(13) NOT NULL,
+  LoanDate DATE,
+  DueDate DATE,
+  ReturnDate DATE,
+  Status BOOLEAN,
+  Fine INT(4),
+  PRIMARY KEY (LoanID),
+  FOREIGN KEY (MemberID) REFERENCES MEMBER(MemberID),
+  FOREIGN KEY (ItemID) REFERENCES ITEM(ItemID)
+);
+
+-- Create EVENTS table
+CREATE TABLE EVENTS (
+  EventID INT(10) NOT NULL AUTO_INCREMENT,
+  SupervisorID INT(6) NOT NULL,
+  Title VARCHAR(255),
+  Description VARCHAR(255),
+  Location SET('First Floor', 'Second Floor', 'Basement'),
+  EDate DATE NOT NULL,
+  PRIMARY KEY (EventID),
+  FOREIGN KEY (SupervisorID) REFERENCES EMPLOYEE(EmployeeID)
+);
